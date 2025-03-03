5371547f4ce8e5ecd1a158e7ead78ee9f54af85d
faluthe <73045509+faluthe@users.noreply.github.com>
2025-03-02 23:14:20 -0500

    get player list, only read symbols once, and marked functions as unsafe instead of blocks

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,17 @@ use process::Process;
 mod player;
 mod process;
 
-fn init() -> anyhow::Result<()> {
+unsafe fn init() -> anyhow::Result<()> {
     // Get the process and localplayer
     let process = Process::new()?;
 
     let player1 = process.get_player1()?;
     info!("Player 1 health: {}", player1.health);
+
+    let players = process.get_players()?;
+    for player in players {
+        info!("Player health: {}", player.health);
+    }
 
     Ok(())
 }
@@ -21,7 +26,7 @@ static INIT: extern "C" fn() = {
         env_logger::Builder::new()
             .filter_level(log::LevelFilter::Debug)
             .init();
-        if let Err(e) = init() {
+        if let Err(e) = unsafe { init() } {
             error!("Initialization error: {}", e);
         }
     }
--- src/process.rs
+++ src/process.rs
@@ -8,6 +8,7 @@ use log::debug;
 use crate::player::Player;
 
 const PLAYER1_SYMBOL: &str = "player1";
+const PLAYERS_SYMBOL: &str = "players";
 
 pub struct Process {
     base_address: u64,
@@ -15,15 +16,35 @@ pub struct Process {
 
 impl Process {
     pub fn new() -> anyhow::Result<Self> {
-        let base_address = Self::main_exe_base().ok_or(anyhow!("Failed to find main exe base"))?;
+        let base_address =
+            unsafe { Self::main_exe_base().ok_or(anyhow!("Failed to find main exe base"))? };
         Ok(Self { base_address })
     }
 
-    pub fn get_player1(&self) -> anyhow::Result<&'static Player> {
-        let offset = self.get_symbol_offset(PLAYER1_SYMBOL)?;
-        let addr = self.base_address + offset;
+    pub unsafe fn get_player1(&self) -> anyhow::Result<&'static Player> {
+        // This is shit but I don't want to use nightly for OnceLock::get_or_try_init
+        static mut OFFSET: u64 = 0;
+        if OFFSET == 0 {
+            OFFSET = self.get_symbol_offset(PLAYER1_SYMBOL)?;
+        }
+        let addr = self.base_address + OFFSET;
 
-        Ok(unsafe { &**(addr as *const *const Player) })
+        Ok(&**(addr as *const *const Player))
+    }
+
+    pub unsafe fn get_players(&self) -> anyhow::Result<Vec<&'static Player>> {
+        static mut OFFSET: u64 = 0;
+        if OFFSET == 0 {
+            OFFSET = self.get_symbol_offset(PLAYERS_SYMBOL)?;
+        }
+        let addr = self.base_address + OFFSET;
+
+        // AssaultCube::vector stores a capacity and a length, we need the length
+        let length = &*((addr + 0xc) as *const u32);
+        // The symbol points to a pointer to a list of Player pointers
+        let list = *(addr as *const *const *const Player);
+        // Localplayer is first, skip it
+        Ok((1..*length).map(|i| &**list.offset(i as isize)).collect())
     }
 
     fn get_symbol_offset(&self, symbol: &str) -> anyhow::Result<u64> {
@@ -42,24 +63,24 @@ impl Process {
         Err(anyhow!("Failed to find symbol: {}", symbol))
     }
 
-    fn main_exe_base() -> Option<u64> {
-        extern "C" fn callback(info: *mut dl_phdr_info, _size: size_t, data: *mut c_void) -> c_int {
-            unsafe {
-                let info = &*info;
-                let base_address = data as *mut u64;
-                if info.dlpi_name.is_null() || *info.dlpi_name == 0 {
-                    *base_address = info.dlpi_addr;
-                    1 // Stop iterating
-                } else {
-                    0
-                }
+    unsafe fn main_exe_base() -> Option<u64> {
+        unsafe extern "C" fn callback(
+            info: *mut dl_phdr_info,
+            _size: size_t,
+            data: *mut c_void,
+        ) -> c_int {
+            let info = &*info;
+            let base_address = data as *mut u64;
+            if info.dlpi_name.is_null() || *info.dlpi_name == 0 {
+                *base_address = info.dlpi_addr;
+                1 // Stop iterating
+            } else {
+                0
             }
         }
 
         let mut base_address: u64 = 0;
-        unsafe {
-            dl_iterate_phdr(Some(callback), &mut base_address as *mut u64 as *mut c_void);
-        }
+        dl_iterate_phdr(Some(callback), &mut base_address as *mut u64 as *mut c_void);
         match base_address {
             0 => None,
             _ => Some(base_address),
