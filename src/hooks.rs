9a2178e3f5b2456b258ea4c1e25f5f2e110f1667
faluthe <73045509+faluthe@users.noreply.github.com>
2025-03-03 21:45:36 -0500

    Hooked SwapWindow

--- src/hooks.rs
+++ src/hooks.rs
@@ -1,10 +1,22 @@
-use std::ffi::CString;
+use std::{
+    ffi::{c_void, CString},
+    ptr,
+};
 
 use anyhow::anyhow;
 use libc::{dlopen, dlsym, RTLD_LAZY, RTLD_NOLOAD};
-use log::debug;
+use log::{debug, warn};
 
 const SDL2_LIB: &str = "/usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0.3000.0";
+
+static mut OG_SWAP_WINDOW: Option<unsafe extern "C" fn(*mut c_void)> = None;
+
+unsafe extern "C" fn hk_swap_window(window: *mut c_void) {
+    debug!("Hooked SDL_GL_SwapWindow!");
+    if let Some(og) = OG_SWAP_WINDOW {
+        og(window);
+    }
+}
 
 pub unsafe fn hook_swap_window() -> anyhow::Result<()> {
     let lib = CString::new(SDL2_LIB)?;
@@ -16,22 +28,29 @@ pub unsafe fn hook_swap_window() -> anyhow::Result<()> {
 
     // The symbol resolves to a wrapper around the actual function
     let fn_name = CString::new("SDL_GL_SwapWindow")?;
-    let sym = {
+    let sym_addr = {
         let sym = dlsym(h_sdl, fn_name.as_ptr());
         if sym.is_null() {
             return Err(anyhow!("Failed to find SDL_GL_SwapWindow"));
         }
         sym as u64
     };
-    debug!("SDL_GL_SwapWindow: {:#X}", sym);
+    debug!("SDL_GL_SwapWindow: {:#X}", sym_addr);
 
     /* SDL_GL_SwapWindow:
      * endbr64            ; (4 bytes)
      * jmp *(ip + offset) ; (2 byte jmp, 4 byte offset) */
-    let offset = *((sym + 0x6) as *const i32);
-    let ip = sym + 0xa;
-    debug!("SDL_GL_SwapWindow GOT offset: {:#X}", offset);
-    debug!("SDL_GL_SwapWindow ip: {:#X}", ip);
+    let offset = ptr::read_unaligned((sym_addr + 0x6) as *const i32) as u64;
+    // Instruction pointer
+    let ip = sym_addr + 0xa;
+    // ip + offset points to the GOT entry for the actual function
+    let fn_got_entry = (ip + offset) as *mut unsafe extern "C" fn(*mut c_void);
+    // Need to call the actual function from the hook
+    OG_SWAP_WINDOW = Some(*fn_got_entry);
+    // The page is already rw so no need to change permissions
+    *fn_got_entry = hk_swap_window;
+    warn!("Hooked SDL_GL_SwapWindow");
 
+    // No dlclose, don't decrement the reference count
     Ok(())
 }
